44439173482eb130fe3a216d75f1f40bf97b2985
x3zvawq <x3zvawq@gmail.com>
2024-05-11 16:18:11 +0800

    perf: 列表拉取逻辑优化

--- index.js
+++ index.js
@@ -4,26 +4,44 @@
  * @version: 1.2.6
  */
 
-import $ from "jquery";
 import JX3BOX from "@jx3box/jx3box-common/data/jx3box.json";
-import {
-    flatMapDeep
-} from "lodash"
-const {
-    __imgPath,
-    __dataPath
-} = JX3BOX;
+import { keyBy } from "lodash";
+const { __imgPath, __dataPath } = JX3BOX;
+
+const queue = [];
+const emotionList = [];
+let loading = false;
 
 // 获取全部表情
-async function getEmotion() {
+async function loadEmotionList() {
+    // 有结果直接过
+    if (emotionList.length > 0) return;
+    // 没结果但是正在loading, 等待不知道谁触发的加载完成
+    if (loading && emotionList.length === 0) {
+        return await new Promise(resolve => {
+            queue.push(resolve);
+        });
+    }
+    // 没结果也没在loading,那就得我来了！
+    loading = true;
+    // 尝试从sessionStorage中获取其他页面的缓存
     const emotion = sessionStorage.getItem("jx3_emotion");
     if (emotion) {
-        return JSON.parse(emotion);
+        emotionList.push(...JSON.parse(emotion));
     } else {
-        const data = await fetch(`${__dataPath}emotion/output/catalog.json`).then((response) => response.json());
-        sessionStorage.setItem("jx3_emotion", JSON.stringify(data));
-        return data;
+        const data = await fetch(
+            `${__dataPath}emotion/output/catalog.json`
+        ).then(response => response.json());
+        // 将二维数组转为一维数组
+        emotionList.push(
+            ...data.map(item => item.items).reduce((a, b) => a.concat(b), [])
+        );
+        sessionStorage.setItem("jx3_emotion", JSON.stringify(emotionList));
     }
+    // 通知等待队列里面的有数据了
+    loading = false;
+    queue.forEach(resolve => resolve());
+    return;
 }
 
 class JX3_EMOTION {
@@ -31,50 +49,28 @@ class JX3_EMOTION {
      * @param {string} joke 骚话字符串
      */
     constructor(joke) {
-        this._joke = $.trim(joke);
-        this.emotionList = [];
-        this.max = 0;
-    }
-
-    // 获取表情
-    async loadEmotionList() {
-        const emotionList = await getEmotion();
-        this.emotionList = emotionList;
+        this.joke = String(joke).trim();
     }
 
     async _renderHTML() {
-        let str = this._joke;
+        // 没有表情符号直接返回
+        if (!this.joke.includes("#")) return this.joke;
 
-        // 如果没有表情符号，直接返回
-        if (!str.includes("#")) return str;
+        await loadEmotionList();
+        const emotionMap = keyBy(emotionList, "key");
 
-        for (let i = 0; i < 3; i++) {
-
-            await this.loadEmotionList();
-
-            const emotions = flatMapDeep(this.emotionList.map(item => item.items));
-            const emotionKeys = emotions.map(item => item.key);
-
-            // 获取表情符号的位置并且替换为img标签
-            while (emotionKeys.some(key => str.includes(key))) {
-                const _index = emotionKeys.findIndex(key => str.includes(key));
-                const key = emotionKeys[_index].replace('#', '$');
-                str = _index > -1 ? str.replace(emotionKeys[_index], key) : str;
+        // 替换表情符号为img标签
+        for (const key in emotionMap) {
+            if (this.joke.includes(key)) {
+                const url = `${__imgPath}emotion/output/${emotionMap[key].filename}`;
+                this.joke = this.joke.replaceAll(
+                    key,
+                    `<img src="${url}" alt="${key}" title="${key}" />`
+                );
             }
-            // 替换表情符号为img标签
-            while (emotionKeys.some(key => str.includes(`${key.replace('#', '$')}`))) {
-                const _index = emotionKeys.findIndex(key => str.includes(key.replace('#', '$')));
-                const _emotion = emotions.find(item => item.key === emotionKeys[_index]);
-                const src = _emotion ? `${__imgPath}emotion/output/${_emotion.filename}` : '';
-                const key = emotionKeys[_index].replace('#', '$');
-                str = src ? str.replace(key, `<img src="${src}" alt="${emotionKeys[_index]}" title="${emotionKeys[_index]}" />`) : str;
-            }
-
-            // 如果已经获取到表情列表，就跳出循环
-            if (this.emotionList.length) break;
         }
 
-        return str;
+        return this.joke;
     }
 }
 
